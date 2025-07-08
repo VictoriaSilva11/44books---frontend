3550edbb1952cd597c0c955289a55e275a7aab7e
VictoriaSilva11 <victoria.silva7@estudante.ifms.edu.br>
2025-07-08 11:17:18 -0300

    Criação e personalização do componente Footer

---
+++ src/Footer.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div className="rodape">
+            <footer>&copy; 44Books - Victória de Almeida Silva - 2025</footer>
+        </div>
+    );
+}
