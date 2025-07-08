fb5862680b459da26a845c9fb19c9ce360cadc4b
VictoriaSilva11 <victoria.silva7@estudante.ifms.edu.br>
2025-07-08 11:16:14 -0300

    Criação de componente da página admin

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,5 +1,21 @@
 import './Admin.css'
+import Header from './Header.tsx'
+import Footer from './Footer.tsx'
 
-function Admin(){}
+function Admin() {
+    return (
+        <>
+            <Header />
+
+            <section className="imagem-section">
+        <img
+          src="https://wallpapercat.com/w/full/4/6/0/34209-3065x1909-desktop-hd-the-lord-of-the-rings-background-image.jpg"
+        />
+      </section>
+
+            <Footer />
+        </>
+    );
+}
 
 export default Admin
