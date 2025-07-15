3c65e957770b13f411351e4d712ab7e2cc23665b
Victória Silva <164199648+VictoriaSilva11@users.noreply.github.com>
2025-07-15 17:06:12 -0400

    tirando historia e pondo home

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Livros from './Livros'
 import Admin from './Admin.tsx'
-import Historia from './Historia'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/Home' element={<Home />} />
         <Route path='/Livros' element={<Livros />} />
         <Route path='/Admin' element={<Admin />} />
-        <Route path='/Historia' element={<Historia />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
